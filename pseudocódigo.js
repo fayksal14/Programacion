38bf9d312b11c49c890d72f67a6f2f19ed2484f3
fayksal14 <faysalkamboui1@hotmail.com>
2025-10-07 16:40:39 +0200

    Añadido primer archivo de la asignatura

---
+++ pseudocódigo.js
@@ -0,0 +1,21 @@
+// PSEUDOCÓDIGO (basado en PseInt)
+
+// Tipos de datos
+// Enteros: 3, 2, 0, -1, ...
+// Reales: 3.0. -1.4, 2.345, ...
+// Caracteres: 'a', 'b', '5', ...
+// Lógicos: sólo pueden tener dos valores: Verdadero y Falso
+
+// Comentarios
+// Esto es un comentario
+
+// Definir o declarar un variable (caja)
+// Definir <var1>, <var2>, <var3>, ..., <varN> como <como tipo de dato>
+
+// Definir Variable altura como real;
+// Definir Edad como enteros;
+
+// Asignación de variables
+// <nombre_variable> <- <valor>
+// <nombre_variable> <- <otra_variable>
+// <nombre_variable> <- <expresion>
